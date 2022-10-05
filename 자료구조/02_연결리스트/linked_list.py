a039822521295364389bfc8400c7935cdc1f157e
Kaeun-Lee <lgeun7026@hanmail.net>
2022-10-05 22:34:48 +0900

    upload linked list

---
+++ 자료구조/02_연결리스트/linked_list.py
@@ -0,0 +1,25 @@
+import random
+
+
+class LinkedListNode:
+    def __init__(self, value, next_node=None, prev_node=None):
+        self.value = value
+        self.next = next_node
+        self.prev = prev_node
+
+    def __str__(self):
+        return str(self.value)
+
+
+class LinkedList:
+    def __init__(self, values=None):
+        self.head = None
+        self.tail = None
+        if values is not None:
+            self.add_multiple(values)
+
+    def __iter__(self):
+        current = self.head
+        while current:
+            yield current  # 결과값을 여러 번 나누어서 제공. generator 반환
+            current = current.next
